f014109371dc58c8db9aab6b090483c9b631872d
Antonio Monteiro <antoniomonteiro@ioasys.com.br>
2019-08-19 19:03:21 -0300

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,34 @@
+package br.com.antoniomonteiro
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            Database.connect("jdbc:mysql://localhost:3306/teste_base", driver = "com.mysql.jdbc.Driver",
+                user = "root", password = "Ahbm101292*")
+
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
